a43994797213e741b1c9583e4c7869b3fc07d97f
JohnnyBackus <john.p.backus@gmail.com>
2024-01-18 15:23:17 -0800

    zilch and hot dice conditions working well

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -3,16 +3,17 @@ from game_logic import GameLogic
 # game variables
 # dice_roller = roller or GameLogic.roll_dice
 round = 1
-total_score = 0
+round_score = 0
+game_score = 0
 dice_remaining = 6
 
-# welcome message
-def play():    
+
+def play(): # welcome message and starts game
     print("Welcome to Ten Thousand")
     start_menu()
 
 
-def start_menu():
+def start_menu(): #separated from welcome message to not repeat welcome in event of invalid response
     print("(y)es to play or (n)o to decline")
     response = input(">")
     if response.lower() not in ["y", "yes", "n", "no"]:
@@ -26,49 +27,96 @@ def start_menu():
 
 
 def start_round_message():
-    print("Starting round ", round, "")
+    global round
+    print("Starting round", round, "")
     game_loop()
 
 
-# game loop to handle each dice roll
-# def game_loop(roller=None, num_rounds=10):
-def game_loop():
-    global dice_remaining
+def quit_game():
+    global game_score
+    print("Thanks for playing. You earned ", game_score, " points")
+    return
+
+
+def play_another_round():
+    print("Play another round? (y)es or (n)o")
+    response = input(">")
+    if response.lower() not in ["y", "yes", "n", "no"]:
+        print("Invalid response. Try again")
+        play_another_round()
+    if response == "y":
+        start_round_message()
+    if response == "n":
+        quit_game()
+
+
+def zilch():
     global round
-    global total_score
-    GameLogic.roll_dice(dice_remaining)
+    global dice_remaining
+    global game_score
+    print("""
+            ****************************************
+            **        Zilch!!! Round over         **
+            ****************************************
+            """)
+    print("You banked 0 points in round", round,"")
+    print("Total score is", game_score, "points")
+    round += 1 # increment round
+    dice_remaining = 6 # reset dice remaining for next round
+    play_another_round()
+    
+
+# def game_loop(roller=None, num_rounds=10):
+def game_loop(): # game loop to handle each dice roll
+    global dice_remaining
+    if dice_remaining == 0:
+        dice_remaining = 6 # hot dice situation, reset dice remaining for next roll
+    # I am pleasantly shocked that the line below didn't break something!
+    max_roll_score = GameLogic.calculate_score(GameLogic.roll_dice(dice_remaining))
+    # print("max_round_score: ", max_round_score)
+    if max_roll_score == 0:
+        zilch()
     print("Enter dice to keep, or (q)uit: ")
     response = input(">")
     if response == "q":
-        print("Thanks for playing. You earned", total_score, "points")
-        return
+        quit_game()
     else:
         kept_dice = list(response)
-        kept_dice = [int(i) for i in kept_dice]
+        kept_dice = [int(i) for i in kept_dice] 
         dice_remaining -= len(kept_dice)
-        # print("kept_dice: ", kept_dice)
+        # print("kept_dice: ", type(kept_dice), kept_dice)
         roll_or_bank(kept_dice)
 
 
-# roll or bank to handle scores and user choice
-def roll_or_bank(dice):
-    global total_score
+def bank():
+    global game_score
+    global round_score
     global dice_remaining
     global round
-    round_score = GameLogic.calculate_score(dice)
-    total_score += round_score
-    print("You have ", round_score, " unbanked points this round and ", dice_remaining, " dice remaining")
+    game_score += round_score # add round score to game score
+    print("You banked", round_score, "points in round", round, "")
+    print("Your total score is", game_score, "points")
+    round += 1 # increment round
+    dice_remaining = 6 #reset dice remaining for next round
+    start_round_message()
+
+
+def roll_or_bank(dice): # roll or bank to handle scores and user choice
+    global game_score
+    global round_score
+    global dice_remaining
+    global round
+    roll_score = GameLogic.calculate_score(dice)
+    print("roll_score: ", roll_score)
+    round_score += roll_score # add points from this roll to round score
+    print("You scored", roll_score, "points this roll and have", round_score, "unbanked points this round with", dice_remaining, "dice remaining")
     print("(r)oll again, (b)ank your points or (q)uit: ")
     response = input(">")
     if response == "q":
-        print("Thanks for playing. You earned ", total_score, " points")
+        print("Thanks for playing. You earned", game_score, "points")
         return
     elif response == "b":
-        print("You banked ", round_score, " in round ", round, "")
-        print("Total score is ", total_score, "points")
-        round += 1 # increment round
-        dice_remaining = 6 #reset dice remaining for next round
-        start_round_message()
+        bank()
     elif response == "r":
         game_loop()
 
@@ -77,6 +125,7 @@ def roll_or_bank(dice):
 if __name__ == "__main__":
 
     play()
+
     # rolls = [(3, 2, 5, 4, 3, 3),
     #          (5, 3, 3, 2, 1, 4),
     # ]
@@ -85,3 +134,5 @@ if __name__ == "__main__":
     #     return [rolls.pop(0)]
     
     # game_loop(roller=mock_roller)
+
+    # print("test calc_score", GameLogic.calculate_score([4, 2, 6]))
