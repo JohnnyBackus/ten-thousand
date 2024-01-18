f6e29ad89b84ab7b56fce9a42ffa4a1aee41bf5e
JohnnyBackus <john.p.backus@gmail.com>
2024-01-18 11:42:19 -0800

    refactored lab07 code

--- ten_thousand/game.py
+++ ten_thousand/game.py
@@ -1,65 +1,80 @@
 from game_logic import GameLogic
 
+# game variables
+# dice_roller = roller or GameLogic.roll_dice
+round = 1
+total_score = 0
+dice_remaining = 6
 
-def play():
-    # game variables
-    round = 1
-    total_score = 0
-    dice_remaining = 6
-
-    def roll_or_bank(kept_dice):
-        nonlocal total_score
-        nonlocal dice_remaining
-        nonlocal round
-        round_score = GameLogic.calculate_score(kept_dice)
-        total_score += round_score
-        print("You have ", round_score, " unbanked points and ", dice_remaining, " dice remaining")
-        print("(r)oll again, (b)ank your points or (q)uit: ")
-        response = input(">")
-        if response == "q":
-            print("Thanks for playing. You earned ", total_score, " points")
-            return
-        elif response == "b":
-            print("You banked ", round_score, " in round ", round, "")
-            round += 1
-            print("Total score is ", total_score, "points")
-            game_loop()
-        elif response == "r":
-            game_loop(dice_remaining)
-
-    
-    def game_loop(dice = 6):
-        nonlocal dice_remaining
-        print("Starting round ", round, "")
-        GameLogic.roll_dice(dice)
-        print("Enter dice to keep, or (q)uit: ")
-        response = input(">")
-        if response == "q":
-            print("Thanks for playing. You earned 0 points")
-            return
-        else:
-            kept_dice = list(response)
-            kept_dice = [int(i) for i in kept_dice]
-            dice_remaining = 6 - len(kept_dice)
-            print("kept_dice: ", kept_dice)
-            roll_or_bank(kept_dice)
-    
-    # welcome message
+# welcome message
+# def play(roller=None, num_rounds=10):
+def play():    
     print("Welcome to Ten Thousand")
+    start_menu()
+
+
+def start_menu():
     print("(y)es to play or (n)o to decline")
     response = input(">")
-    if response not in ["y", "n"]:
+    if response.lower() not in ["y", "yes", "n", "no"]:
         print("Invalid response. Try again")
-        play()
+        start_menu()
     if response == "n":
         print("OK. Maybe another time")
         return
     elif response == "y":
         game_loop()
-    
-  
 
+
+def game_loop():
+        global dice_remaining
+        global round
+        global total_score
+        print("Starting round ", round, "")
+        GameLogic.roll_dice(dice_remaining)
+        print("Enter dice to keep, or (q)uit: ")
+        response = input(">")
+        if response == "q":
+            print("Thanks for playing. You earned", total_score, "points")
+            return
+        else:
+            kept_dice = list(response)
+            kept_dice = [int(i) for i in kept_dice]
+            dice_remaining -= len(kept_dice)
+            # print("kept_dice: ", kept_dice)
+            roll_or_bank(kept_dice)
+
+
+def roll_or_bank(dice):
+    global total_score
+    global dice_remaining
+    global round
+    round_score = GameLogic.calculate_score(dice)
+    total_score += round_score
+    print("You have ", round_score, " unbanked points and ", dice_remaining, " dice remaining")
+    print("(r)oll again, (b)ank your points or (q)uit: ")
+    response = input(">")
+    if response == "q":
+        print("Thanks for playing. You earned ", total_score, " points")
+        return
+    elif response == "b":
+        print("You banked ", round_score, " in round ", round, "")
+        round += 1
+        print("Total score is ", total_score, "points")
+        game_loop()
+    elif response == "r":
+        game_loop()
     
+      
 
 if __name__ == "__main__":
+
     play()
+    # rolls = [(3, 2, 5, 4, 3, 3),
+    #          (5, 3, 3, 2, 1, 4),
+    # ]
+    
+    # def mock_roller(num_dice):
+    #     return [rolls.pop(0)]
+    
+    # play(roller=mock_roller)
